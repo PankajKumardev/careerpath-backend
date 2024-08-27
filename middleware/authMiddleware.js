130deeb38c04feed6d41683e30bb9171a26b8856
pankaj kumar <Pankajams1234@gmail.com>
2024-08-28 00:44:32 +0530

    fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization']; // Use 'headers' and ensure it's in lower case
+    const authHeader = req.headers['authorization']; 
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401); // Unauthorized if no token
+    if (token == null) return res.sendStatus(401); 
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403); // Forbidden if token is invalid
-        req.user = user; // Attach the user to the request
-        next(); // Proceed to the next middleware or route handler
+        if (err) return res.sendStatus(403); 
+        req.user = user;
+        next(); 
     });
 };
 
